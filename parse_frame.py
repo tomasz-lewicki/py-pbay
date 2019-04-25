6de95e6112d61ee872e6372292d35cb1baefcb8b
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-04-25 07:17:48 +0100

    parsing to a dict working

---
+++ parse_frame.py
@@ -0,0 +1,49 @@
+import re
+import time
+import serial
+import serial.threaded
+
+init_values = {
+'IRR': None, 
+'LTM': None, 
+'HDT': None,
+'LPP': None,
+'AT0': None,
+'CMO': None, 
+'SHT': None, 
+'OZO': None, 
+'SHH': None, 
+'BAD': None, 
+'AT2': None, 
+'SQN': None, 
+'SO2': None, 
+'HDH': None, 
+'NO2': None, 
+'AT3': None,
+'AT1': None,
+'IAQ': None,
+'SIR': None,
+'H2S': None,
+'SUV': None,
+'SVL': None,
+'LPT': None
+}
+
+class PBayParser(serial.threaded.LineReader):
+    def __init__(self):
+        super(PBayParser, self).__init__()
+        self.values = init_values
+        self.data = []
+        self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=[0-9]+')
+
+    def handle_line(self, data):
+        keyvalue = self._p.findall(data)
+        for kv in keyvalue:
+            self.values[kv[0:3]] = kv[5:]
+        
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
+
+with serial.threaded.ReaderThread(ser, PBayParser) as reader:
+    while True:
+        print(reader.values)
+        time.sleep(1)
