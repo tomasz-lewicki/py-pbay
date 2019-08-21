7150e272226c89bfa0ac93e1cdd2ba7ae092f744
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-08-21 11:01:39 -0700

    fixed regex bug that did not pick on non-numeric board address and sequence no

--- pbay2.py
+++ pbay2.py
@@ -16,29 +16,37 @@ class PBay(Thread):
         super(PBay, self).__init__()
         self.measurements = INIT_VALUES
         self._s = serial.Serial(port=serial_address, baudrate=115200, timeout=5) 
-        self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=[0-9]+')
+        self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=[0-9|A-Z]+')
     
     def run(self):
-        while True:
-            print()
+        while self._is_running:
             l = str(self._s.readline())
+            print(l)
             self.parse_line(l)
 
     def parse_line(self, data):
         keyvalue = self._p.findall(data)
         for kv in keyvalue:
+            print(kv)
             self.measurements[kv[0:3]] = kv[4:]
         
     def __enter__(self):
+        self._is_running = True
         self.start()
         while None in self.measurements.values():
             #wait for first batch of measurements to come
             time.sleep(1)
+            print("waiting for measurements...")
+            print(self.measurements)
         return self
 
     def __exit__(self, etype, value, traceback):
         print(etype,value,traceback)
         self._s.close()
+        self._is_running = False
+
+    def stop(self):
+        self._is_running = False
 
 
 if __name__ == '__main__':
@@ -49,7 +57,6 @@ if __name__ == '__main__':
         logfile.write('timestamp,CMO,H2S,IAQ,IRR,OZO,SO2,NO2\n')
 
     with PBay('/dev/ttyUSB0') as sensor, open(filename, 'w') as logfile:
-        
         print(sensor.measurements['CMO'])
         logfile.write(
             str(round(time.time()))+','
@@ -62,6 +69,6 @@ if __name__ == '__main__':
             + sensor.measurements['NO2']+','
             + '\n'
             )
-            
+        
         time.sleep(5)
-
+        
