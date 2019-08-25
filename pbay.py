9d081a83c79473ac002fe3d52c120478ae03292f
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-08-24 23:14:40 -0700

    substituted pbay2.py for pbay.py

--- pbay.py
+++ pbay.py
@@ -1,72 +1,108 @@
 import re
 import time
-import serial
-import serial.threaded
+import serial #pip3 install pyserial
+import logging
+from serial.serialutil import SerialException
+from threading import Thread
 
-init_values = {
-    'IRR': None, 
-    'LTM': None, 
-    'HDT': None,
-    'LPP': None,
-    'AT0': None,
-    'CMO': None, 
-    'SHT': None, 
-    'OZO': None, 
-    'SHH': None, 
-    'BAD': None, 
-    'AT2': None, 
-    'SQN': None, 
-    'SO2': None, 
-    'HDH': None, 
-    'NO2': None, 
-    'AT3': None,
-    'AT1': None,
-    'IAQ': None,
-    'SIR': None,
-    'H2S': None,
-    'SUV': None,
-    'SVL': None,
-    'LPT': None
+INIT_VALUES = {
+    'IRR': None, 'LTM': None, 'HDT': None, 'LPP': None, 'AT0': None,
+    'CMO': None, 'SHT': None, 'OZO': None, 'SHH': None, 'BAD': None, 
+    'AT2': None, 'SQN': None, 'SO2': None, 'HDH': None, 'NO2': None, 
+    'AT3': None, 'AT1': None, 'IAQ': None, 'SIR': None, 'H2S': None,
+    'SUV': None, 'SVL': None, 'LPT': None
 }
 
-class PBayParser(serial.threaded.LineReader):
-    def __init__(self):
-        super(PBayParser, self).__init__()
-        self.values = init_values
-        self.data = []
-        self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=[0-9]+')
+class PBay(Thread):
+    def __init__(self, serial_address, log_filename):
+        super(PBay, self).__init__()
+        self.measurements = INIT_VALUES
+        self._s = serial.Serial(port=serial_address, baudrate=115200, timeout=5) 
+        self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=[0-9|A-Z]+') #BUG: doesn't handle negative SO2
 
-    def handle_line(self, data):
-        keyvalue = self._p.findall(data)
-        for kv in keyvalue:
-            self.values[kv[0:3]] = kv[4:]
+        # configure logger
+        self._log = logging.getLogger('pbay_driver')
+        self._log.setLevel(logging.DEBUG)
+        # create file handler which logs even debug messages
+        fh = logging.FileHandler(log_filename)
+        fh.setLevel(logging.DEBUG)
+        # create console handler with a higher log level
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        # add the handlers to the logger
+        self._log.addHandler(fh)
+        self._log.addHandler(ch)
+
+    
+    def run(self):
+        while self._is_running:
+            l = str(self._s.readline())
+            self._log.debug("Recived a line: " + l)
+            self.parse_line(l)
+            self._log.info("Updated values: {}".format(self.measurements))
+            #Wrap it in try except once you know how to handle these
+            #try:
+            #except SerialException as e: #TODO: add error handling
+            #    self._log.error(e) 
+
+    def parse_line(self, data):
+        keyvalue_pairs = self._p.findall(data)
+        for kv in keyvalue_pairs:
+            key = kv[0:3]
+            if key in INIT_VALUES.keys():
+                self.measurements[key] = kv[4:]
+            else:
+                self._log.warning("Received an erronous key from the device: " + key)
+        
+    def __enter__(self):
+        self._is_running = True
+        self.start()
+        while None in self.measurements.values():
+            #wait for first batch of measurements to come
+            missing_values = [k for k, v in self.measurements.items() if v == None]
+            self._log.info("waiting for measurements... Still waiting for: {}".format(missing_values)) 
+            time.sleep(1)
+        self._log.info("Got all the measurements. Starting.") 
+        return self
+
+    def __exit__(self, etype, value, traceback):
+        print("Exit called")
+        print(etype,value,traceback)
+        self._is_running = False
+        self.join() #TODO: is this right?
+        self._s.close()
+
+    def stop(self):
+        self._is_running = False
+
 
 if __name__ == '__main__':
-    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-    filename = 'log'+ str(round(time.time())) +'.csv'
-    with open(filename, 'w') as file:
-        file.write('timestamp,CMO,H2S,IAQ,IRR,OZO,SO2,NO2\n')
-    
-    #BUG: exits on any exception in reader thread. Should be a daemon that keep trying
+    csvfilename = 'readings.csv'
+    csvheader = 'timestamp,CMO,H2S,IAQ,IRR,OZO,SO2,NO2\n'
 
-    with serial.threaded.ReaderThread(ser, PBayParser) as reader: 
-        while True:
+    with open(csvfilename, 'a+') as csvfile:
+        if csvfile.readline() != csvheader:
+            csvfile.write(csvheader)
 
-            if None not in reader.values.values(): #don't output to csv until all values are populated
+    with PBay('/dev/ttyUSB0', 'log.txt') as sensor:
+        while(True):
+            with open(csvfilename, 'a') as csvfile:
 
-                with open(filename, 'a') as file:
-                    file.write(
-                        str(round(time.time()))+','
-                        + reader.values['CMO']+','
-                        + reader.values['H2S']+','
-                        + reader.values['IAQ']+','
-                        + reader.values['IRR']+','
-                        + reader.values['OZO']+','
-                        + reader.values['SO2']+','                  
-                        + reader.values['NO2']+','
-                        + '\n'
-                        )
+                csvfile.write(
+                    str(round(time.time()))+','
+                    + sensor.measurements['CMO']+','
+                    + sensor.measurements['H2S']+','
+                    + sensor.measurements['IAQ']+','
+                    + sensor.measurements['IRR']+','
+                    + sensor.measurements['OZO']+','
+                    + sensor.measurements['SO2']+','                  
+                    + sensor.measurements['NO2']+'\n'
+                    )
             
             time.sleep(5)
-
+            
