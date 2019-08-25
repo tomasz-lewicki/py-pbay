ac9740775f79fc3e162fde63e93a88aca8da0537
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-08-24 23:11:42 -0700

    added updates to old pbay

--- pbay.py
+++ pbay.py
@@ -4,29 +4,29 @@ import serial
 import serial.threaded
 
 init_values = {
-'IRR': None, 
-'LTM': None, 
-'HDT': None,
-'LPP': None,
-'AT0': None,
-'CMO': None, 
-'SHT': None, 
-'OZO': None, 
-'SHH': None, 
-'BAD': None, 
-'AT2': None, 
-'SQN': None, 
-'SO2': None, 
-'HDH': None, 
-'NO2': None, 
-'AT3': None,
-'AT1': None,
-'IAQ': None,
-'SIR': None,
-'H2S': None,
-'SUV': None,
-'SVL': None,
-'LPT': None
+    'IRR': None, 
+    'LTM': None, 
+    'HDT': None,
+    'LPP': None,
+    'AT0': None,
+    'CMO': None, 
+    'SHT': None, 
+    'OZO': None, 
+    'SHH': None, 
+    'BAD': None, 
+    'AT2': None, 
+    'SQN': None, 
+    'SO2': None, 
+    'HDH': None, 
+    'NO2': None, 
+    'AT3': None,
+    'AT1': None,
+    'IAQ': None,
+    'SIR': None,
+    'H2S': None,
+    'SUV': None,
+    'SVL': None,
+    'LPT': None
 }
 
 class PBayParser(serial.threaded.LineReader):
@@ -40,14 +40,33 @@ class PBayParser(serial.threaded.LineReader):
         keyvalue = self._p.findall(data)
         for kv in keyvalue:
             self.values[kv[0:3]] = kv[4:]
-        
 
 if __name__ == '__main__':
     ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-    #TODO: change this so it's only PBayParser
+    filename = 'log'+ str(round(time.time())) +'.csv'
+    with open(filename, 'w') as file:
+        file.write('timestamp,CMO,H2S,IAQ,IRR,OZO,SO2,NO2\n')
+    
+    #BUG: exits on any exception in reader thread. Should be a daemon that keep trying
+
     with serial.threaded.ReaderThread(ser, PBayParser) as reader: 
         while True:
-            print('H2S', reader.values['H2S'], 'SO2', reader.values['SO2'], 'NO2', reader.values['NO2'], 'OZO: ' ,reader.values['OZO'])
+
+            if None not in reader.values.values(): #don't output to csv until all values are populated
+
+                with open(filename, 'a') as file:
+                    file.write(
+                        str(round(time.time()))+','
+                        + reader.values['CMO']+','
+                        + reader.values['H2S']+','
+                        + reader.values['IAQ']+','
+                        + reader.values['IRR']+','
+                        + reader.values['OZO']+','
+                        + reader.values['SO2']+','                  
+                        + reader.values['NO2']+','
+                        + '\n'
+                        )
+            
             time.sleep(5)
 
