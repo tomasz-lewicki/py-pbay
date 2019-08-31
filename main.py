5d83699a8a86b7e4009b73707f716108f0c50c5c
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-08-30 21:48:41 -0700

    pep8

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import sys
 import time
-from pbay import PBay, INIT_VALUES, Gas #TODO: hide gases implementation
+from pbay import PBay, INIT_VALUES, Gas  # TODO: hide gases implementation
 
 if __name__ == '__main__':
-    #Usage example: python3 pbay.py /dev/ttyUSB1
+    # Usage example: python3 pbay.py /dev/ttyUSB1
     csvfilename = 'readings2.csv'
     devname = sys.argv[-1]
 
-    #write header
+    # write header
     with open(csvfilename, 'a+') as csvfile:
         csvfile.write('timestamp,')
         for k in INIT_VALUES.keys():
             csvfile.write(k+',')
         csvfile.write('\n')
 
-    gas_list = [Gas(name='CMO', calib_curr=9851, calib_temp=29.20, nanoamps_per_ppm=4.75, coef=12, temp_source='AT0')]
+    gas_list = [Gas(name='CMO', calib_curr=9851, calib_temp=29.20,
+                    nanoamps_per_ppm=4.75, coef=12, temp_source='AT0')]
 
-    #open sensor and write the values
+    # open sensor and write the values
     with PBay(serial_address=devname, log_filename='log.txt', gas_list=gas_list) as sensor:
         while(sensor._is_running):
             with open(csvfilename, 'a') as csvfile:
@@ -24,8 +25,6 @@ if __name__ == '__main__':
                 # csvfile.write(str(round(time.time())) + ',')
                 # for val in sensor.measurements.values():
                 #     csvfile.write(val + ',')
-                
                 # csvfile.write('\n')
-            
+
             time.sleep(5)
-            
--- pbay/__init__.py
+++ pbay/__init__.py
@@ -3,36 +3,37 @@ import time
 import math
 from collections import OrderedDict
 import re
-import serial #pip3 install pyserial
+import serial  # pip3 install pyserial
 import logging
 from serial.serialutil import SerialException
 from threading import Thread
 
 INIT_VALUES = OrderedDict({
-    #Board address, sequence number
+    # Board address, sequence number
     'BAD': None, 'SQN': None,
 
-    #sensor currents
+    # sensor currents
     'CMO': None, 'SO2': None, 'NO2': None, 'H2S': None,
     'OZO': None, 'IAQ': None, 'IRR': None,
 
-    #temperatures
-    'SHT': None,  'LPT': None, 'HDT': None, 
-    'AT0': None, #CO ADC
-    'AT1': None, #IAQ/IRR ADC temp
-    'AT2': None, #O3/NO2 ADC temp
-    'AT3': None, #SO2/H2S ADC temp
-    'LTM': None, #CO LMP Temp
+    # temperatures
+    'SHT': None,  'LPT': None, 'HDT': None,
+    'AT0': None,  # CO ADC
+    'AT1': None,  # IAQ/IRR ADC temp
+    'AT2': None,  # O3/NO2 ADC temp
+    'AT3': None,  # SO2/H2S ADC temp
+    'LTM': None,  # CO LMP Temp
 
-    #humidity
+    # humidity
     'SHH': None, 'HDH': None,
 
-    #pressure 
+    # pressure
     'LPP': None,
-     
-    #Si1145 sensor (light)
-    'SIR': None, 'SUV': None, 'SVL': None, 
+
+    # Si1145 sensor (light)
+    'SIR': None, 'SUV': None, 'SVL': None,
 })
+
 
 class Gas(object):
     def __init__(self, name, calib_curr, calib_temp, temp_source, nanoamps_per_ppm, coef):
@@ -41,7 +42,8 @@ class Gas(object):
         self.calib_temp = calib_temp
         self.temp_source = temp_source
         self.picoamps_per_ppm = 1000 * nanoamps_per_ppm
-        self.coef = coef #TODO:better name?
+        self.coef = coef  # TODO:better name?
+
     def __str__(self):
         return self.name
 
@@ -53,17 +55,18 @@ class Gas(object):
 #             Gas(name=, calib_curr=, calib_temp=, nanoamps_per_ppm=, coef=),
 #             Gas(name=, calib_curr=, calib_temp=, nanoamps_per_ppm=, coef=)]
 
+
 class PBay(Thread):
     def __init__(self, serial_address, log_filename, gas_list):
         super(PBay, self).__init__()
-        #initialize state
+        # initialize state
         self._raw = INIT_VALUES
         self._values = {}
         self._gases = gas_list
         self._is_running = False
 
-        #initialize peripherals
-        self._s = serial.Serial(port=serial_address, baudrate=115200, timeout=5) 
+        # initialize peripherals
+        self._s = serial.Serial(port=serial_address, baudrate=115200, timeout=5)
         self._p = re.compile('-?[A-Z][A-Z|0-9][A-Z|0-9]{1}=-?[0-9|A-Z]+')
 
         # configure logger
@@ -85,73 +88,73 @@ class PBay(Thread):
 
     def __enter__(self):
 
-        #wait for first batch of _raw to come
+        # wait for first batch of _raw to come
         while None in self._raw.values():
-            l = self._read_sensor()
-            kv = self._parse(l)
+            li = self._read_sensor()
+            kv = self._parse(li)
             self._update_state(kv)
 
-            missing_values = [k for k, v in self._raw.items() if v == None]
-            self._log.info("Waiting for _raw... Still waiting for: {}".format(missing_values)) 
+            missing_values = [k for k, v in self._raw.items() if v is None]
+            self._log.info("Waiting for _raw... Still waiting for: {}".format(missing_values))
 
         self._log.info("Got all the parameters. Starting.")
         self._calculate_values()
         self._is_running = True
         self.start()
         return self
-    
+
     def run(self):
         while self._is_running:
             try:
-                l = self._read_sensor()
-                kv = self._parse(l)
+                li = self._read_sensor()
+                kv = self._parse(li)
                 self._update_state(kv)
                 self._calculate_values()
             except SerialException as e:
                 self._log.critical("Device disconnected with Exception: {}".format(e))
                 self._is_running = False
-                #TODO: add some better error handling? Chances are that it will reconnect and change /dev/tty*
-    
+                # TODO: add some better error handling?
+                # Chances are that it will reconnect and change /dev/tty*
+
     def _read_sensor(self):
-        l = str(self._s.readline())
-        self._log.debug("Recived a line: " + l)
-        return l
+        li = str(self._s.readline())
+        self._log.debug("Recived a line: " + li)
+        return li
 
     def _parse(self, data):
         keyvalue_pairs = self._p.findall(data)
         return keyvalue_pairs
-    
+
     def _update_state(self, keyvalue_pairs):
         for kv in keyvalue_pairs:
             key = kv[0:3]
             if key in INIT_VALUES.keys():
                 if key == 'BAD' or key == 'SQN':
                     self._raw[key] = kv[4:]
-                else: self._raw[key] = float(kv[4:])
+                else:
+                    self._raw[key] = float(kv[4:])
             else:
                 self._log.warning("Received an erronous key from the device: " + key)
 
     def _calculate_values(self):
         for g in self._gases:
             deltaT = self._raw[g.temp_source]/100 - g.calib_temp
-            I_net =  self._raw[g.name] - g.calib_temp * pow(math.e, deltaT/g.coef)
+            I_net = self._raw[g.name] - g.calib_temp * pow(math.e, deltaT/g.coef)
             self._values[g.name] = I_net/g.picoamps_per_ppm
         self._log.debug("Calculated values" + str(self._values))
-
 
     def __getattr__(self, attr):
         gas_names = list(map(str, self._gases))
         if attr not in gas_names:
-            raise AttributeError('{} has no attribute {}. Maybe one of: {}?'.format(self, attr, gas_names))
+            raise AttributeError(
+                '{} has no attribute {}.Maybe one of: {}?'.format(self, attr, gas_names))
         else:
             return self._values[attr]
 
     def __exit__(self, etype, value, traceback):
         self._is_running = False
-        self.join() #TODO: is this right?
+        self.join()  # TODO: is this right?
         self._s.close()
 
     def stop(self):
         self._is_running = False
-
-
